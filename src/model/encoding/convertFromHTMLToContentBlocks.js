11025cc55c643e3ed53fd6e3131513adac51a7ca
sugarshin <shinsugar@gmail.com>
2016-04-21 11:15:11 +0900

    fix `convertFromHTMLToContentBlocks`

--- src/model/encoding/convertFromHTMLToContentBlocks.js
+++ src/model/encoding/convertFromHTMLToContentBlocks.js
@@ -155,7 +155,7 @@ function getBlockDividerChunk(block: DraftBlockType, depth: number, node: ?Node)
     }],
   };
   if (block === 'checkable-list-item' && hasInputTypeCheckbox(node)) {
-    ret.blocks[0].checked = node.children[0].checked ? true : false;
+    chunk.blocks[0].checked = node.children[0].checked ? true : false;
   }
   return chunk;
 }
